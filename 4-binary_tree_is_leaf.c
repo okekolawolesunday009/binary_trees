6bf7b3c0e27ce4bfa73d02b020c39af2ee4e5de3
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-10-31 12:28:34 +0100

    code

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,5 +1,13 @@
 #include "binary_trees.h"
 
 int binary_tree_is_leaf(const binary_tree_t *node){
-    
+	/**code**/
+	while (node)
+	{
+		if (node == NULL)
+			return (0);
+		if (node->right || node->left)
+		    return (0);
+	}
+    return (1);
 }
