350c081929decc8b2d98e878409a1c58cb72b102
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-10-31 03:05:46 +0100

    first commit

---
+++ 0-binary_tree_node.c.c
@@ -0,0 +1,9 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value) {
+    binary_tree_t* newnode = malloc(sizeof(binary_tree_t));
+    newnode->n = value;
+    newnode->left = NULL;
+    newnode->right = NULL;
+    return newnode;
+}
