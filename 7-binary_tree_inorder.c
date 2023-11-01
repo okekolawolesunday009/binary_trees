3b3a6384cbe07651b670391fbf00082b9e92923e
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-11-01 03:29:29 -0700

    7

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -8,10 +8,9 @@
 */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (tree || func == NULL)
-        return;
-   
-    binary_tree_preorder(tree->left, func);
-    func(tree->n);
-    binary_tree_preorder(tree->right, func);  
+	if (tree == NULL || func == NULL)
+		return;
+	binary_tree_preorder(tree->left, func);
+	func(tree->n);
+	binary_tree_preorder(tree->right, func);
 }
--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,16 +1,15 @@
 #include "binary_trees.h"
 /**
- * binary_tree_preorder - prints the dtree in postorder traversal
+ * binary_tree_postorder - prints the dtree in postorder traversal
  * @tree: pinter th head node
  * @func: function pointer int
  * Return: always print result
 */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (tree || func == NULL)
-        return;
-   
-    binary_tree_preorder(tree->left, func);
-    binary_tree_preorder(tree->right, func);  
-    func(tree->n);
+	if (tree == NULL || func == NULL)
+		return;
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+	func(tree->n);
 }
