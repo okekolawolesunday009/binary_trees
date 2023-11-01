031195e4d21f13c8786a63c5348a1ebf15a23e4d
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-11-01 04:12:44 -0700

    code

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,33 +7,26 @@
 */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	const binary_tree_t *left, *right;
-    left = NULL;
-    right = NULL;
-    size_t h_l = 0;
-    size_t h_r =  0;
-    
-    if (tree == NULL)
-        return (0);
-    while (tree)
-    {
-        if (tree->left )
-        {
-            left = tree->left;
-            h_l = binary_tree_height(tree->left) + 1;
-        }
-        else
-            h_l = 0;
-        if (tree->right)
-        {
-            right = tree->right;
-            h_r = binary_tree_height(tree->right) + 1;
-        }
-        else
-            h_r = 0;
-        if (h_l < h_r)
-            return (h_r);
+
+	size_t h_l = 0;
+	size_t h_r =  0;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left)
+	{
+		h_l = binary_tree_height(tree->left) + 1;
+	}
+	else
+		h_l = 0;
+	if (tree->right)
+	{
+		h_r = binary_tree_height(tree->right) + 1;
+	}
+	else
+		h_r = 0;
+	if (h_l < h_r)
+		return (h_r);
 	else
 		return (h_l);
-    }
 }
