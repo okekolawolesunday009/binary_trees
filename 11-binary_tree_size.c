4c5d182b5d3fc239745a14a0df6d17e0a220e408
OkeKolawoleSunday009 <okekolawolesunday@gmail.com>
2023-11-02 18:12:34 -0700

    11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -11,10 +11,9 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	size_t size = 0;
 
 	if (tree == NULL)
-		return (NULL);
+		return (0);
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 	size =  binary_tree_size(tree->right) + 1 + binary_tree_size(tree->left);
 	return (size);
-	}
 }
